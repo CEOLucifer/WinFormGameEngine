441c18d8ae104cbc13ae684939db4a87ed39086d
WWZ <wwz2832918775@163.com>
2023-12-06 16:02:12 +0800

    BattleUIPanel取消动画，帧数变高了

--- Assets/Scripts/UI/BattleUIPanel.cs
+++ Assets/Scripts/UI/BattleUIPanel.cs
@@ -15,10 +15,8 @@ using 炮打飞机.Assets.Form;
 public class BattleUIPanel : BaseComponent, IUIPanel
 {
     private UIControl<BattleUIControl> m_c_turretBtnsPanel;
-    private LerpMove m_lerpMove;
 
     public UIControl<BattleUIControl> C_turretBtnsPanel { get => m_c_turretBtnsPanel; set => m_c_turretBtnsPanel = value; }
-    public LerpMove LerpMove { get => m_lerpMove; set => m_lerpMove = value; }
 
     public void PanelHide()
     {
@@ -33,15 +31,7 @@ public class BattleUIPanel : BaseComponent, IUIPanel
         // 作为子物体
         obj.Transform.Parent = this.Transform;
 
-        Form form = GameSys.Instance.Form;
         BattleUIControl control = m_c_turretBtnsPanel.Control;
-
-        // 动画
-        m_lerpMove = this.AddComponent<LerpMove>();
-        m_lerpMove.EndPos = new Vector2(form.Width - control.Width, form.Height - control.Height);
-
-        // 设置初始位置
-        this.Transform.Position = new Vector2(form.Width + 500, form.Height - control.Height);
 
         control.Dock = DockStyle.Fill;
     }
